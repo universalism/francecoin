360a81620f198dd3d9777c32dd59efdf8d4788ea
universalism <universalism@protonmail.com>
2017-08-30 21:58:22 +0200

    rename data dir

--- src/util.cpp
+++ src/util.cpp
@@ -488,7 +488,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Litecoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Francecoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -498,10 +498,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Litecoin";
+    return pathRet / "Library/Application Support/Francecoin";
 #else
     // Unix
-    return pathRet / ".litecoin";
+    return pathRet / ".Francecoin";
 #endif
 #endif
 }
